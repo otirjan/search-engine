a1efcc02dfde1d4703d625f2a8f6f24bef50cc92
camerontofani <152201676+camerontofani@users.noreply.github.com>
2024-04-10 17:08:41 -0500

    starting doc parser

---
+++ DocParser.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+// RapidJSON headers we need for our parsing.
+#include "rapidjson/istreamwrapper.h"
+#include "rapidjson/document.h"
+
+using namespace rapidjson;
+using namespace std;
+
+// void testReadJsonFile(const string &fileName);
+// void testFileSystem(const string &path);   idk what these are i think supposed to read in from file 
+
