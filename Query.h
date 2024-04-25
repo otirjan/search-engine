0575b25c2939e692f1dc041fb6a7749b337577cf
camerontofani <152201676+camerontofani@users.noreply.github.com>
2024-04-25 16:53:14 -0500

    stemming and tokenizing the query, need to implement processQuery

--- Query.h
+++ Query.h
@@ -8,16 +8,20 @@
 #include "DocParser.h"
 
         class QueryProcessor{
+            private:
+                IndexHandler& handler;
+                set<string> stopwords;
+            
             public:
                 //take in the users string, breaks it into what terms are people/orgs/words, gets results from avl trees, ranks them on relevancy
-                //take same index handler u used for doc parser, acsess to the same data 
                 QueryProcessor(IndexHandler& handler); 
                
                //take in the query, return a list of documents 
                 std::vector<std::string> processQuery(const std::string& query);
 
-            private:
-                IndexHandler& handler;
+                std::vector<std::string> tokenize (const std::string& text);
+                std::string stemWord(std::string& word);
+                void initializeStopWords();
 
         };
 
