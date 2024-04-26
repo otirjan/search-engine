7419f6590a877c891e4c26f3508aa123d18d00d2
Makayla Loretta Alston <malston@genuse62.smu.edu>
2024-04-25 21:09:05 -0500

    starting ui

---
+++ userinterface.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <chrono>
+#include "AVLTree.h"
+
+/*
+needs to:
+- index
+- index from file
+- query
+- view full article
+- output time taken for 1 query
+- return how many articles are in the index
+- any other interesting stats
+- a menu
+
+*/
