1ed5daf77b95ef40c29e357a9c37c9463cd1072c
Makayla Loretta Alston <malston@genuse62.smu.edu>
2024-05-01 03:13:13 -0500

    polished the comments

--- main.cpp
+++ main.cpp
@@ -21,14 +21,14 @@ int main(int argc, char* argv[])
     //create a string variable to read in the command from the command line interface
     std::string command = argv[1];
 
+    //intializes string variables filepath and query_string to hold input from terminal
     std::string filepath;
     std::string query_string;
 
     //if the command is index
     if (command == "index")
     {
-        //saves the filepath provided in a string called filepath 
-        //std::string filepath = argv[2];
+        //saves the filepath
         filepath = argv[2];
         //calls UI's index function that accepts a string parameter
         search.index(filepath);
@@ -36,12 +36,11 @@ int main(int argc, char* argv[])
     //if the command is query
     if (command == "query")
     {
-        //saves the query terms provided in a string called query_string
-        //std::string query_string = argv[2];
+        //saves the query terms
         query_string = argv[2];
-        //std::cout << "query string: " << query_string << std::endl;
-        //calls UI's query function that accepts a string parameter
+        //constructs index from csv files
         search.AVLfromFile();
+        //calls UI's query function that accepts a string parameter
         search.query(query_string);
     }
     //if the command is ui
@@ -50,7 +49,6 @@ int main(int argc, char* argv[])
         //calls UI's menu, which allows the user to index, collapse to csv, build from csv, query, view an article and view stats at their leisure
         search.menu();
     }
-
 
     return 0;
 }
