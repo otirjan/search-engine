70a7eae1da47cb6d220ea2c13043d3941ccd100a
Makayla Loretta Alston <malston@genuse62.smu.edu>
2024-05-01 02:09:38 -0500

    when i get input from terminal it does not fill the AVL

--- main.cpp
+++ main.cpp
@@ -13,11 +13,44 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-    
+    //create an instance of the UI class
     UI search;
-    search.menu();
+
+    //create a string variable to read in the command from the command line interface
+    std::string command = argv[1];
+
+    std::string filepath;
+    std::string query_string;
+
+    //if the command is index
+    if (command == "index")
+    {
+        //saves the filepath provided in a string called filepath
+        //std::string filepath = argv[2];
+        filepath = argv[2];
+        //calls UI's index function that accepts a string parameter
+        search.index(filepath);
+    }
+    //if the command is query
+    if (command == "query")
+    {
+        //saves the query terms provided in a string called query_string
+        //std::string query_string = argv[2];
+        query_string = argv[2];
+        //std::cout << "query string: " << query_string << std::endl;
+        //calls UI's query function that accepts a string parameter
+        search.query(query_string);
+    }
+    //if the command is ui
+    if (command == "ui")
+    {
+        //calls UI's menu, which allows the user to index, collapse to csv, build from csv, query, view an article and view stats at their leisure
+        search.menu();
+    }
+
 
     return 0;
 }
+
